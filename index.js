6d40fc2455c8e1d121f25d84d1f81383ab1f4912
Linus Fackler <78768883+linusfackler@users.noreply.github.com>
2024-02-27 00:32:18 -0600

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,10 @@ client.on('ready', () => {
     console.log('The bot is online');
 })
 
-const CHANNELS = ['1207477005902418022'];
+const CHANNELS = ['1210005194134392873'];
 const IGNORE_PREFIX = ['!', '.', ','];
+
+const IGNORE_DUMBASSES = ['428343552007864348', '262764303214575617', '279654000759144448', '484151643932196877'];
 
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_KEY,
@@ -19,16 +21,29 @@ const openai = new OpenAI({
 })
 
 client.on('messageCreate', async (message) => {
+    // if (IGNORE_DUMBASSES.includes(message.author.id)) {
+    //     var temp = message.content;
+    //     let mixedCaseStr = '';
+    //     for (let i = 0; i < temp.length; i++) {
+    //         mixedCaseStr += Math.random() > 0.5 ? temp[i].toUpperCase() : temp[i].toLowerCase();
+    //     }
+    //     mixedCaseStr += ":nerd:"
+    //     message.reply(mixedCaseStr);
+    //     return;
+    // }
+    
     if (message.author.bot) return;
     if (IGNORE_PREFIX.some(prefix => message.content.startsWith(prefix))) return;
     if (!CHANNELS.includes(message.channelId)) return;
+
+
 
     const response = await openai.chat.completions.create({
         model: 'gpt-3.5-turbo-0125',
         messages: [
             {
                 role: 'system',
-                content: 'You are the worlds best music genre decider. Just using “yes” or “no”, you have to decide if the following band/artist qualifies as indie. Alternative bands, such as Radiohead, are not indie. If you don\'t recognize the band, say "I don\'t recognize this band/artist."'
+                content: 'You are the worlds best music genre decider. Just using “yes” or “no”, you have to decide if the following band/artist qualifies as indie. Alternative bands, such as Radiohead or Weezer, as well as hip hop or rap artists, such as MF DOOM, are not indie. Arctic Monkeys is indie. If you don\'t recognize the band, say "I don\'t recognize this band/artist. If the artist is Taylor Swift, say "die". If the artist is Radiohead, say "uhhh umm uhh uhhhhh".'
             },
             {
                 role: 'user',
